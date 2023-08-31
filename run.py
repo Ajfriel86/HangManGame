952c1d59d59f834ed3891f1ff329593c10bc205f
Angus Friel <angus.friel.1986@gmail.com>
2023-08-31 09:27:59 +0100

    Add import module for generating random words

--- run.py
+++ run.py
@@ -1,3 +1,2 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import the random module for generating random words
+import random
