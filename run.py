d3ec746cd66a4235e9f2dcb7606cbb17969351a8
Angus Friel <angus.friel.1986@gmail.com>
2023-09-01 11:08:04 +0100

    Add pylint ot extensions in VS code, found 63 errors relating to the doc string comments, removed these and add '#' comments as well as double backslashes on the hangman figure

--- run.py
+++ run.py
@@ -1,47 +1,93 @@
-# Import the random module for generating random words
+"""
+Import the random module 
+for generating random words
+"""
 import random
+# This is the class that
+# defines the hangman game
 
 
-class Hangman:  # This is the class that defines the hangman game
-    # this is a the definition of a new function, __init__ lets the class initialize the object's attributes, self, is a refrenece to the instance of the object, while words & level are the valuees being passed when the instance is being created
+class Hangman:
+    """
+    # This is the definition of a new function, __init__ lets
+    # the class initialize the object's attributes
+    # self, is a reference to the instance of the object,
+    # while words & level are the values being passed when the
+    # instance is being created
+    """
+
     def __init__(self, words, level):
-        # This stores the list of words that are passed to the hangman game (object)
+        """
+        This stores the list of words 
+        that are passed to the h
+        angman game (object)
+        """
         self.words = words
-        # This stores the level selected
+
+        """
+        This stores the 
+        level selected
+        """
         self.level = level
-        # Choose a random word of the level selcted
+        """
+        Choose a random word 
+        of the level-selected
+        """
         self.word = self.choose_word()
-        #  This creates an empty list, when the user guesses a leeter it is added to the list
+
+        """" 
+        This creates an empty list, when the user guesses a letter it is added to the list
+        """
         self.guessed_letters = []
-        # Number of attempts the user has
+        """
+        Number of attempts the user has
+        """
         self.attempts = 6
 
-    # This function selects a random word from the array list of preselected words depending on the level selected
+    # This function selects a random word from the
+    # array list of preselected words depending
+    # on the level selected
     def choose_word(self):
-        # This filters the words depending on their length and then matchs that with the level selected
+        """ 
+        This filters the words depending on 
+        their length and then matches 
+        that with the level selected
+        """
         filtered_words = [
             word for word in self.words if len(word) == self.level]
-        # This returns a random word fropm the filtered words
+
+        # This returns a random word from the filtered words
         return random.choice(filtered_words)
 
-    # This function is for generating a string that represents the word the user is trying to guess
+    # This function is for generating a string that represents the word
+    # the user is trying to guess
     def display_word(self):
-        # This initilizes an empty string
+        """
+        This initializes an empty string
+        """
         display = ""
-        # This is a loop that iterate through each letter
+        # This is a loop that iterates
+        # through each letter
         for letter in self.word:
-            # This checks if the letter selected is correct
+            # This checks if the letter
+            # selected is correct
             if letter in self.guessed_letters:
                 # if it is correct it is displayed
                 display += letter
             else:
                 # If it is not correct the underscore remains
                 display += "_"
-        # This returns the word to be guessed with underscores fif it is inccorect
+        # This returns the word to be
+        # guessed with underscores if
+        # it is incorrect
         return display
 
-        # This functions displays the various stages of the hangman game
     def display_hangman(self):
+        """
+        This function displays the
+        various stages of the hangman game
+        """
+
         # This is the list of hangman stages
         hang = ["""
         
@@ -76,73 +122,100 @@ class Hangman:  # This is the class that defines the hangman game
     +---+
     |   |
     O   |
-    /|\  |
+    /|\\  |
         |
         |
     =========""", """
     +---+
     |   |
     O   |
-    /|\  |
+    /|\\ |
     /    |
         |
     =========""", """
     +---+
     |   |
     O   |
-    /|\  |
-    / \  |
+    /|\\  |
+    / \\ |
         |
     ========="""]
-        # This dispalys/retunrs the hangman figure based on the attempts taken by the user
+
+        # This displays/returns the hangman
+        # figure based on the attempts taken
+        # by the user
+
         return hang[6 - self.attempts]
 
-    # This function is for the core logic of the game. It is responsible for the gameplay loop where the user guesses letter from the hidden word
+    # This function is for the core logic of the game.
+    # It is responsible for the gameplay loop where
+    # the user guesses a letter from the hidden word
     def play(self):
-        # This is a text dispaly
+        """
+        This is a text display
+        """
         print("Welcome to Hangman!")
 
-        # This is the gameplay loop that ensure the games keeps playing until all conditions are met
+        # This is the gameplay loop that ensures
+        # the game keeps playing until all
+        # conditions are met
         while True:
             # Display the hangman figure
             print(self.display_hangman())
+
             # Display the letters when guessed correctly
             print("\n" + self.display_word())
-            # This is a prompt for thr user to input a letter and stores the guessed letter in the varibale 'guess'
+
+            # This is a prompt for the user to input
+            # a letter and store's the guessed letter
+            # in the variable 'guess'
             guess = input("Guess a letter: ").lower()
 
-            # This if statement is for ensuring only 1 letter was entered
+            # This if statement is for ensuring only
+            # 1 letter was entered
             if len(guess) != 1 or not guess.isalpha():
                 print("Please enter a single letter.")
                 continue
 
-            # This if statement checks if a letter has been selected already
+            # This if statement checks if a
+            # letter has been selected already
             if guess in self.guessed_letters:
                 print("You already guessed that letter.")
                 continue
 
-            # If the letter selected meets the above criteria it is added to the variable 'guess'
+            # If the letter selected meets the above
+            # criteria it is added to the variable 'guess'
             self.guessed_letters.append(guess)
 
-            # If the guessed letter is in the hidden word - 'Correct!' - is dispalyed
+            # If the guessed letter is in the
+            # hidden word - 'Correct!' - is displayed
             if guess in self.word:
                 print("Correct!")
-                # Else - Worg - and the number of attempts left is displayed
+                # Else - Worg - and the number of attempts
+                # left is displayed
             else:
                 self.attempts -= 1
                 print(f"Wrong! You have {self.attempts} attempts left.")
 
-            # This if statement checks if all underscores (hidden letters) have been guessed correctly and if so, a congrats message is displayed
+            # This if statement checks if all underscores
+            # (hidden letters) have been guessed correctly
+            # and if so, a congrats message is displayed
             if "_" not in self.display_word():
                 print("Congratulations, you've guessed the word!")
                 break
 
-            # This if statement checks if the user runs out of tries and dispalys a message if they lsot and the word they were trying to guess
+            # This if statement checks if the user
+            # runs out of tries and displays a message
+            # if they lost and the word they were trying
+            # to guess
             if self.attempts == 0:
                 print("Sorry, you've run out of attempts. The word was:", self.word)
                 break
 
-            # At the end of the game a prompt is displayed to ask the user if they wish to play again or leave the game
+            # At the end of the
+            # game a prompt is displayed to ask
+            # the user if they wish to play again
+            # or leave the game
         play_again = input("Do you want to play again? (y/n): ").lower()
         if play_again == 'y':
             return True
@@ -151,14 +224,17 @@ class Hangman:  # This is the class that defines the hangman game
             return False
 
 
-# Each level contains different word lengths and they are stored here
-words = {
+# Each level contains different word
+# lengths and they are stored here
+wordsDic = {
     "easy": ["cat", "dog", "bat", "hat", "pen"],
     "medium": ["apple", "banana", "cherry", "grape", "lemon"],
     "hard": ["elephant", "giraffe", "kangaroo", "zebra", "ostrich"]
 }
 
-# Each level is reffered to by its first letter and this corresponds to the level and word length for that level
+
+# Each level is referred to by its first letter and this
+# corresponds to the level and word length for that level
 level_table = {
     "E": ("Easy", "3 Letter words"),
     "M": ("Medium", "5 Letter words"),
@@ -166,7 +242,11 @@ level_table = {
 }
 
 
-def display_level_table():  # This function is for displaying a table for the level code, level type, and the word length of the level
+def display_level_table():
+    """
+    This function is for displaying a table for the level code,
+    level type, and the word length of the level
+    """
     print("| Code | Level  | Description        |")
     print("|------|--------|--------------------|")
     for code, (level, description) in level_table.items():
@@ -174,62 +254,69 @@ def display_level_table():  # This function is for displaying a table for the le
     print("|------|--------|--------------------|")
 
 
-def main():  # This is the main function of Hangman game, handling the game setup, level selection, gameplay, and whether the user wants to play again.
-    # This while loop is to keep the game running
+# This is the main function of the Hangman game,
+# handling the game setup, level selection, gameplay,
+# and whether the user wants to play again.
+def main():
+    """
+    This while loop is to keep the game running
+    """
     while True:
-        # This is to display the available levels for the user to choose from
+        # This is to display the available levels
+        # for the user to choose from
         display_level_table()
         # This is a prompt for the user to choose a level
         level_choice = input(
             "Choose a level code (E for Easy, M for Medium, H for Hard): ").upper()
-        # This checks if the chosen level selected is valid
+        # This checks if the chosen level is valid
         if level_choice in level_table:
             # This gets the chosen level from the level_table
             chosen_level, _ = level_table[level_choice]
             # This displays the selected by the user
             print(f"You have chosen the '{chosen_level}' level.")
-
-            # This is an if/elif statement to determine the level selected by the user
-            # Check if the chosen level is easy
+            # This is an if/elif statement to determine
+            # the level selected by the user.
+            # And checks if the chosen level is easy
             if level_choice == "E":
                 # Selects the list of words for the easy level
-                level_words = words["easy"]
+                level_words = wordsDic["easy"]
                 # Sets the word length for the easy level
                 word_length = 3
-            # Checks if the chosen level is medium
+                # Checks if the chosen level is medium
             elif level_choice == "M":
-                # Selects the list of words for the medium level
-                level_words = words["medium"]
-                # Sets the word length for the medium level
+                # Selects the list of words for the medium-level
+                level_words = wordsDic["medium"]
+                # Sets the word length for the medium-level
                 word_length = 5
                 # If the chosen level is not easy or medium, it's hard
             else:
-                # Selects the list of words for the hard level
-                level_words = words["hard"]
-                # Sets the word length for the hard level
+                # Selects the list of words for the hard-level
+                level_words = wordsDic["hard"]
+                # Sets the word length for the hard-level
                 word_length = 7
             # Display game instructions
             print(
                 f"Instructions: You have 6 tries to guess a {word_length}-letter word.")
-
-            # Creates a Hangman game instance with the chosen level's words and word length
+            # Creates a Hangman game instance with
+            # the chosen level's words and word length
             game = Hangman(level_words, word_length)
-            # Starts playing the game and gets whether the user wants to play again
+            # Starts playing the game and gets
+            # whether the user wants to play again
             play_again = game.play()
-
             # Checks if the user doesn't want to play again
             if not play_again:
                 # Displays a farewell message
                 print("Thanks for playing! Goodbye.")
                 # Exits the loop to end the game
                 break
-        # If the level selcted is incorrect
+            # If the level selected is incorrect
         else:
             # Display an error message
             print("Invalid level choice.")
 
 
-# This if statement ensures the main() function is executed when the script is run directly
+# This if statement ensures the main()
+# function is executed when the script is run directly
 if __name__ == "__main__":
     # Starts the main game loop if this script is run directly
     main()
