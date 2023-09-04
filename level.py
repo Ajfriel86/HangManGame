4cd00718e85bd43fbb28378e41d1a06568b56aa8
Angus Friel <angus.friel.1986@gmail.com>
2023-09-04 10:01:12 +0100

    Add game level structure to seperate file to be called

---
+++ level.py
@@ -0,0 +1,23 @@
+"""
+This file conatins the level structure
+And the table that displays the levels and word count 
+for each level
+It is then imported into run.py
+"""
+level_table = {
+    "E": ("Easy", "3 Letter words"),
+    "M": ("Medium", "5 Letter words"),
+    "H": ("Hard", "7 Letter words")
+}
+
+
+def display_level_table():
+    """
+    This function is for displaying a table for the level code,
+    level type, and the word length of the level
+    """
+    print("| Code | Level  | Description        |")
+    print("|------|--------|--------------------|")
+    for code, (level, description) in level_table.items():
+        print(f"| {code}    | {level:<6} | {description:<18} |")
+    print("|------|--------|--------------------|")
