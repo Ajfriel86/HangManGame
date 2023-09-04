f8a454c1572bb3cc165f22a5af74ef22ebfdb229
Angus Friel <angus.friel.1986@gmail.com>
2023-09-04 10:44:19 +0100

    Run code through https://pep8ci.herokuapp.com/ and correct errors

--- words.py
+++ words.py
@@ -12,27 +12,28 @@ wordsDic = {
         "hop", "ink", "jug", "lip", "net", "pot", "rug", "toy", "ice", "bus"
     ],
     "medium": [
-        "apple", "banana", "cherry", "grape", "lemon", "mango", "peach", "pear", "melon", "apricot",
-        "orange", "plum", "kiwi", "berry", "coconut", "fig", "papaya", "pineapple",
-        "pomegranate", "raspberry",
-        "strawberry", "watermelon", "cucumber", "carrot", "broccoli", "cauliflower",
-        "potato", "tomato", "spinach", "lettuce",
-        "cabbage", "radish", "zucchini", "onion", "garlic", "pepper", "eggplant",
-        "mushroom", "asparagus", "celery",
-        "kale", "peas", "beans", "corn", "squash", "yam", "turnip", "beet", "leek", "okra"
+        "apple", "banana", "cherry", "grape", "lemon", "mango", "peach",
+        "pear", "melon", "apricot",
+        "orange", "plum", "kiwi", "berry", "coconut", "fig", "papaya",
+        "pineapple", "pomegranate", "raspberry",
+        "strawberry", "watermelon", "cucumber", "carrot", "broccoli",
+        "cauliflower", "potato", "tomato", "spinach", "lettuce",
+        "cabbage", "radish", "zucchini", "onion", "garlic", "pepper",
+        "eggplant", "mushroom", "asparagus", "celery",
+        "kale", "peas", "beans", "corn", "squash", "yam", "turnip",
+        "beet", "leek", "okra"
     ],
     "hard": [
-        "elephant", "giraffe", "kangaroo", "zebra", "ostrich", "rhinoceros", "hippopotamus",
-        "crocodile",
+        "elephant", "giraffe", "kangaroo", "zebra", "ostrich", "rhinoceros",
+        "hippopotamus", "crocodile",
         "alligator", "chimpanzee",
-        "orangutan", "gorilla", "platypus", "cheetah", "leopard", "jaguar", "puma", "panther",
-        "hyena",
-        "wildebeest",
-        "antelope", "gazelle", "vulture", "condor", "pelican", "seagull", "albatross", "kingfisher",
-        "toucan", "woodpecker",
-        "scorpion", "tarantula", "centipede", "millipede", "mantis", "praying mantis", "dragonfly",
-        "damselfly", "earthworm", "termite",
-        "leech", "mosquito", "tick", "flea", "locust", "grasshopper", "caterpillar", "butterfly",
-        "firefly", "ladybug"
+        "orangutan", "gorilla", "platypus", "cheetah", "leopard", "jaguar",
+        "puma", "panther", "hyena", "wildebeest",
+        "antelope", "gazelle", "vulture", "condor", "pelican", "seagull",
+        "albatross", "kingfisher", "toucan", "woodpecker",
+        "scorpion", "tarantula", "centipede", "millipede", "mantis",
+        "praying mantis", "dragonfly", "damselfly", "earthworm", "termite",
+        "leech", "mosquito", "tick", "flea", "locust", "grasshopper",
+        "caterpillar", "butterfly", "firefly", "ladybug"
     ]
 }
