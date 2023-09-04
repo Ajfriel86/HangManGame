31e053b28735a98bc61d559e76917275fad183f2
Angus Friel <angus.friel.1986@gmail.com>
2023-09-04 10:01:57 +0100

    Add the words dictionary to seperate file to be called

---
+++ words.py
@@ -0,0 +1,38 @@
+"""
+Each level contains different word lengths and they are stored here
+This file is then imported to the run.py file
+There are 50 words for each level
+"""
+wordsDic = {
+    "easy": [
+        "cat", "dog", "hat", "pen", "rat", "cup", "bed", "car", "sun", "egg",
+        "cow", "box", "key", "pie", "ant", "man", "toy", "fan", "bee", "top",
+        "bus", "bat", "fox", "cap", "bag", "nut", "map", "web", "van", "pig",
+        "mat", "can", "owl", "pan", "rug", "jam", "leg", "hat", "gum", "gun",
+        "hop", "ink", "jug", "lip", "net", "pot", "rug", "toy", "ice", "bus"
+    ],
+    "medium": [
+        "apple", "banana", "cherry", "grape", "lemon", "mango", "peach", "pear", "melon", "apricot",
+        "orange", "plum", "kiwi", "berry", "coconut", "fig", "papaya", "pineapple",
+        "pomegranate", "raspberry",
+        "strawberry", "watermelon", "cucumber", "carrot", "broccoli", "cauliflower",
+        "potato", "tomato", "spinach", "lettuce",
+        "cabbage", "radish", "zucchini", "onion", "garlic", "pepper", "eggplant",
+        "mushroom", "asparagus", "celery",
+        "kale", "peas", "beans", "corn", "squash", "yam", "turnip", "beet", "leek", "okra"
+    ],
+    "hard": [
+        "elephant", "giraffe", "kangaroo", "zebra", "ostrich", "rhinoceros", "hippopotamus",
+        "crocodile",
+        "alligator", "chimpanzee",
+        "orangutan", "gorilla", "platypus", "cheetah", "leopard", "jaguar", "puma", "panther",
+        "hyena",
+        "wildebeest",
+        "antelope", "gazelle", "vulture", "condor", "pelican", "seagull", "albatross", "kingfisher",
+        "toucan", "woodpecker",
+        "scorpion", "tarantula", "centipede", "millipede", "mantis", "praying mantis", "dragonfly",
+        "damselfly", "earthworm", "termite",
+        "leech", "mosquito", "tick", "flea", "locust", "grasshopper", "caterpillar", "butterfly",
+        "firefly", "ladybug"
+    ]
+}
