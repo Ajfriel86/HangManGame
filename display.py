8ecfdd6c912d76cb5e6cffacbcc9cf6dfdf53e54
Angus Friel <angus.friel.1986@gmail.com>
2023-09-04 10:00:16 +0100

    Add the hangman figure display to a seperate file to be called

---
+++ display.py
@@ -0,0 +1,83 @@
+"""
+This is a file that contains the hangman
+figure and is then imported into run.py
+"""
+
+
+def display_hangman(attempts):
+    """
+    This function displays the
+    various stages of the hangman game
+    based on the number of attempts left.
+    """
+    # This is the list of hangman stages
+    hang = [
+        """
+    
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+        /|   |
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+        /|\\|
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+        /|\\|
+        /   |
+            |
+        =========
+        """,
+        """
+        +--+
+        |   |
+        O   |
+        /|\\|
+        / \\|
+            |
+        =========
+        """
+    ]
+
+    # This displays/returns the hangman
+    # figure based on the attempts taken
+    return hang[6 - attempts]
