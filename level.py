54a5eb9ab1c0eed7b23c38b5ef4217b5078b5141
Angus Friel <angus.friel.1986@gmail.com>
2023-10-14 11:43:21 +0100

    Add colorama to level selection

--- level.py
+++ level.py
@@ -4,6 +4,8 @@ And the table that displays the levels and word count
 for each level
 It is then imported into run.py
 """
+from colorama import Fore
+
 level_table = {
     "E": ("Easy", "3 Letter words"),
     "M": ("Medium", "5 Letter words"),
@@ -16,8 +18,8 @@ def display_level_table():
     This function is for displaying a table for the level code,
     level type, and the word length of the level
     """
-    print("| Code | Level  | Description        |")
+    print(f"{Fore.GREEN}| Code | Level  | Description        |")
     print("|------|--------|--------------------|")
     for code, (level, description) in level_table.items():
-        print(f"| {code}    | {level:<6} | {description:<18} |")
-    print("|------|--------|--------------------|")
+        print(f"|{Fore.GREEN}{code}     | {level:<6} | {description:<18} |")
+    print(f"{Fore.GREEN}|------|--------|--------------------|")
